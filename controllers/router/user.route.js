e1c13bd702438d1016148cc13d2a5142909eaabe
Md Liton Mahmud <litonmahmud0023@gmail.com>
2024-02-08 03:10:46 +0600

    create delete and singout api route

--- controllers/router/user.route.js
+++ controllers/router/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { test, updateUser } from '../user.controllers.js';
+import { deleteUser, test, updateUser } from '../user.controllers.js';
 import { verifyToken } from '../utiles/veryfyUser.js';
 
 
@@ -7,6 +7,7 @@ const router= express.Router();
 
 router.get('/test',test)
 router.post('/update/:id',verifyToken,updateUser)
+router.delete('/delete/:id',verifyToken,deleteUser)
 
 
 export default router;
